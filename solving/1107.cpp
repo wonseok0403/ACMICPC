e3aadaab96d995c9c100407ad868712d50a83958
장원석 <wonseok@jang-wonseog-ui-MacBook-Air.local>
2018-08-16 11:19:37 +0900

    8.15

--- solving/1107.cpp
+++ solving/1107.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
 #include<stdio.h>
 using namespace std;
-int N;
+int N, jarisu = 1;
 bool chk[10] ={1,}; // chk = 1 -> okay
+int MIN = 500200;
+int recursive(int n, int i){
+    if( i == jarisu ) {
+        return N-n;
+    }
+    else{
+        int tmp;
+        for(int j=0; j<=9; j++){
+            if(chk[j]){
+                tmp = recursive(n*10+j, i+1);
+                if( tmp < MIN ){
+                    MIN = tmp;
+                }
+            }
+        }
+    }    
+}
 int main(){
     cin >> N;
     int m, tmp;
@@ -11,4 +28,11 @@ int main(){
         cin >> tmp;
         chk[tmp] = false;
     }
+    tmp = N;
+    while( tmp/10 == 0 ){
+        jarisu ++;
+        tmp/=10;
+    }
+    recursive(0, 0);
+    cout << MIN;
 }
